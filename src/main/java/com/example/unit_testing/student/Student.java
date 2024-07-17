d5571e2f9cf9bbd29e4e9b3da3c6ef4ebe1d7402
KhomotsoJerry <jerry@khomotso.com>
2024-07-17 13:46:11 +0200

    added addStudent , delete , anf find all student unit test

--- src/main/java/com/example/unit_testing/student/Student.java
+++ src/main/java/com/example/unit_testing/student/Student.java
@@ -42,6 +42,14 @@ public class Student implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public Student(String firstName, String lastName, String email, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
--- src/test/java/com/example/unit_testing/StudentServiceTest.java
+++ src/test/java/com/example/unit_testing/StudentServiceTest.java
@@ -1,17 +1,29 @@
 package com.example.unit_testing;
 
+import com.example.unit_testing.student.Role;
+import com.example.unit_testing.student.Student;
 import com.example.unit_testing.studentRepository.StudentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.verify;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
     @Mock
@@ -32,11 +44,53 @@ class StudentServiceTest {
     }
 
     @Test
-    void addStudent() {
+    void CanAddStudent() {
+        // given
+        Student student = new Student("jerry","thulare","jerry@gmail.com","123456", Role.USER);
+        when(studentRepository.findStudentByEmail(student.getEmail())).thenReturn(true);
+        underTest.addStudent(student);
+        // when
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        Student capturedStudent = studentArgumentCaptor.getValue();
+        // then
+       assertThat(capturedStudent).isEqualTo(student);
+    }
+    @Test
+    public void willThrowErrorWhenAddingStudent(){
+      // given
+        Student student = new Student("jerry","thulare","jerry@gmail.com","123456", Role.USER);
+         when(studentRepository.findStudentByEmail(student.getEmail())).thenReturn(false);
+
+         // when
+
+         assertThatThrownBy(()-> underTest.addStudent(student))
+                 .isInstanceOf(UsernameNotFoundException.class)
+                 .hasMessageContaining("user not found");
+         verify(studentRepository , never()).save(any());
     }
 
     @Test
     void deleteStudent() {
+        // given
+        Student student = new Student("jerry","thulare","jerry@gmail.com","123456", Role.USER);
+        when(studentRepository.findById(anyLong())).thenReturn(Optional.of(student));
+        // when
+        underTest.deleteStudent(student.getId());
+        //then
+        verify(studentRepository).deleteById(any());
+    }
+    @Test
+    public void willThrownAnErrorWhenDeletingStudent(){
+        // given
+        Student student = new Student("jerry","thulare","jerry@gmail.com","123456", Role.USER);
+       when(studentRepository.findById(anyLong())).thenReturn(Optional.empty());
+       // when
+        assertThatThrownBy(()-> underTest.deleteStudent(student.getId()))
+                .isInstanceOf(UsernameNotFoundException.class)
+                .hasMessageContaining("not on the file");
+        // then
+        verify(studentRepository,never()).deleteById(any());
     }
 
     @Test
